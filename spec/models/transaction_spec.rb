d71ffe2c8f4d9ee6f88ddd8afab9d62f55119490
Benji Thompson <thompson.benji@gmail.com>
2023-04-27 19:10:27 -0400

    feat: add test for Transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:from_address).class_name('Address').with_foreign_key('from_address').with_primary_key('address') }
+    it { should belong_to(:to_address).class_name('Address').with_foreign_key('to_address').with_primary_key('address') }
+  end
 end
