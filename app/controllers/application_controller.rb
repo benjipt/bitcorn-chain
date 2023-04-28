a629d317299c71e14e04585a5e59bd3d2dbbbaec
Benji Thompson <thompson.benji@gmail.com>
2023-04-28 15:50:35 -0400

    feat: api can process payload in camelCase or snake_case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :transform_request_parameters
+
+  private
+
+  def transform_request_parameters
+    params.transform_keys!(&:underscore)
+    params.deep_transform_keys!(&:underscore)
+  end
 end
--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe TransactionsController, type: :controller do
   let(:satoshi) { Address.create(address: 'satoshi kozuka', cornlet_balance: 100_000_000_000_000) }
+  let!(:from_address) { create(:address, address: 'from_address', cornlet_balance: 10000) }
+  let!(:to_address) { create(:address, address: 'to_address', cornlet_balance: 0) }
 
   describe 'POST #create' do
     context 'with valid parameters' do
@@ -92,6 +94,26 @@ RSpec.describe TransactionsController, type: :controller do
         it 'returns an unprocessable entity status' do
           post :create, params: missing_to_address_params
           expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context 'with snake_case payload' do
+        it 'creates a transaction successfully' do
+          expect {
+            post :create, params: { transaction: { from_address: 'from_address', to_address: 'to_address', amount: 1000 } }
+          }.to change(Transaction, :count).by(1)
+  
+          expect(response).to have_http_status(:created)
+        end
+      end
+  
+      context 'with camelCase payload' do
+        it 'creates a transaction successfully' do
+          expect {
+            post :create, params: { transaction: { fromAddress: 'from_address', toAddress: 'to_address', amount: 1000 } }
+          }.to change(Transaction, :count).by(1)
+  
+          expect(response).to have_http_status(:created)
         end
       end
     end
