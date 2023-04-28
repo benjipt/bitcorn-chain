132c7c992f62eb7da6453ab3852a564f0f895bde
Benji Thompson <thompson.benji@gmail.com>
2023-04-28 17:27:05 -0400

    feat: convert unit from bitcorn to cornlet on POST transactions/ and add additional error handling

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,20 +3,42 @@ class TransactionsController < ApplicationController
     transaction_payload = params.require(:transaction).permit(:from_address, :to_address, :amount)
     from_address = Address.find_by(address: transaction_payload[:from_address])
     to_address = transaction_payload[:to_address].present? ? Address.find_or_create_by(address: transaction_payload[:to_address]) : nil
-    amount = transaction_payload[:amount].to_i
+
+    if transaction_payload[:amount].nil?
+      render json: { error: "Amount is required" }, status: :unprocessable_entity
+      return
+    end
 
     if from_address.nil?
-      render json: { error: "From address not found" }, status: :unprocessable_entity
+      render json: { error: "fromAddress is required" }, status: :unprocessable_entity
       return
     end
 
     if to_address.nil?
-      render json: { error: "To address is required" }, status: :unprocessable_entity
+      render json: { error: "toAddress is required" }, status: :unprocessable_entity
       return
     end
+    
+    # Convert amount to decimal
+    amount_decimal = transaction_payload[:amount].to_d
+
+    # Check if amount is valid
+    if amount_decimal <= 0
+      render json: { error: "Amount should be greater than 0" }, status: :unprocessable_entity
+      return
+    end
+    
+    # Check if amount has more than 6 digits to the right of the decimal point
+    if amount_decimal.frac.to_s.split('.')[1].size > 6
+      render json: { error: "Amount can have no more than 6 digits to the right of the decimal point" }, status: :unprocessable_entity
+      return
+    end
+    
+    # Convert unit from bitcorn to cornlet (1 bitcorn == 1_000_000 cornlets)
+    amount = (amount_decimal * 1_000_000).to_i
 
     if from_address.cornlet_balance < amount
-      render json: { error: "insufficient cornlet_balance" }, status: :unprocessable_entity
+      render json: { error: "insufficient balance" }, status: :unprocessable_entity
       return
     end
 
---
+++ spec/requests/transactions_spec.rb
@@ -0,0 +1,178 @@
+# spec/requests/transactions_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Transactions', type: :request do
+  let!(:from_address) { create(:address, address: 'from_address', cornlet_balance: 10_000_000) }
+  let!(:to_address) { create(:address, address: 'to_address', cornlet_balance: 0) }
+
+  describe 'POST /transactions' do
+    context 'with valid parameters' do
+      let(:valid_params) do
+        {
+          transaction: {
+            fromAddress: 'from_address',
+            toAddress: 'to_address',
+            amount: '4'
+          }
+        }
+      end
+
+      it 'creates a new transaction' do
+        expect {
+          post transactions_path, params: valid_params, as: :json
+        }.to change(Transaction, :count).by(1)
+      end
+
+      it 'returns a success response and a success message' do
+        post transactions_path, params: valid_params, as: :json
+        expect(response).to have_http_status(:created)
+        expect(JSON.parse(response.body)).to eq('status' => 'success', 'message' => 'Transaction created successfully')
+      end
+
+      it 'updates the from_address and to_address cornlet_balance' do
+        post transactions_path, params: valid_params, as: :json
+        expect(from_address.reload.cornlet_balance).to eq(6_000_000)
+        expect(to_address.reload.cornlet_balance).to eq(4_000_000)
+      end
+
+      it 'creates a transaction with the correct cornlet_amount' do
+        post transactions_path, params: valid_params, as: :json
+        expect(Transaction.last.cornlet_amount).to eq(4_000_000)
+      end
+    end
+
+    context 'with insufficient balance' do
+      let(:insufficient_balance_params) do
+        {
+          transaction: {
+            fromAddress: 'from_address',
+            toAddress: 'to_address',
+            amount: '20'
+          }
+        }
+      end
+
+      it 'does not create a new transaction' do
+        expect {
+          post transactions_path, params: insufficient_balance_params, as: :json
+        }.not_to change(Transaction, :count)
+      end
+
+      it 'returns an unprocessable_entity response with an insufficient balance error message' do
+        post transactions_path, params: insufficient_balance_params, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq('error' => 'insufficient balance')
+      end
+    end
+
+    context 'with missing fromAddress' do
+      let(:missing_from_address_params) do
+        {
+          transaction: {
+            toAddress: 'to_address',
+            amount: '4'
+          }
+        }
+      end
+
+      it 'does not create a new transaction' do
+        expect {
+          post transactions_path, params: missing_from_address_params, as: :json
+        }.not_to change(Transaction, :count)
+      end
+
+      it 'returns an unprocessable_entity response with a missing fromAddress error message' do
+        post transactions_path, params: missing_from_address_params, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq('error' => 'fromAddress is required')
+      end
+    end
+
+    context 'with missing toAddress' do
+      let(:missing_to_address_params) do
+        {
+          transaction: {
+            fromAddress: 'from_address',
+            amount: '4'
+          }
+        }
+      end
+
+      it 'does not create a new transaction' do
+        expect {
+          post transactions_path, params: missing_to_address_params, as: :json
+        }.not_to change(Transaction, :count)
+      end
+
+      it 'returns an unprocessable_entity response with a missing toAddress error message' do
+        post transactions_path, params: missing_to_address_params, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq('error' => 'toAddress is required')
+      end
+    end
+
+    context 'with missing amount' do
+      let(:missing_amount_params) do
+        {
+          transaction: {
+            fromAddress: 'from_address',
+            toAddress: 'to_address'
+          }
+        }
+      end
+
+      it 'does not create a new transaction' do
+        expect {
+          post transactions_path, params: missing_amount_params, as: :json
+        }.not_to change(Transaction, :count)
+      end
+
+      it 'returns an unprocessable_entity response with a missing amount error message' do
+        post transactions_path, params: missing_amount_params, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq('error' => 'Amount is required')
+      end
+    end
+
+    context 'with amount less than or equal to 0' do
+      let(:negative_amount_params) do
+        {
+          transaction: {
+            fromAddress: 'from_address',
+            toAddress: 'to_address',
+            amount: '-1'
+          }
+        }
+      end
+      let(:zero_amount_params) do
+        {
+          transaction: {
+            fromAddress: 'from_address',
+            toAddress: 'to_address',
+            amount: '0'
+          }
+        }
+      end
+
+      it 'does not create a new transaction' do
+        expect {
+          post transactions_path, params: negative_amount_params, as: :json
+        }.not_to change(Transaction, :count)
+        expect {
+          post transactions_path, params: zero_amount_params, as: :json
+        }.not_to change(Transaction, :count)
+      end
+
+      it 'returns an unprocessable_entity response with an amount less than 0 error message' do
+        post transactions_path, params: negative_amount_params, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq('error' => 'Amount should be greater than 0')
+
+        post transactions_path, params: zero_amount_params, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq('error' => 'Amount should be greater than 0')
+      end
+    end
+
+  end
+end
