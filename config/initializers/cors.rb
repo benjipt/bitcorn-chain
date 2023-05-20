c25ce98ac47907c24ef0e0066b2eda1fb2034cc5
Benji Thompson <thompson.benji@gmail.com>
2023-05-20 13:56:42 -0400

    refactor[cors.rb]: run rubocop

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,8 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins ENV['LOCAL_ORIGIN'], ENV['PRODUCTION_ORIGIN']
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             credentials: false
   end
 end
