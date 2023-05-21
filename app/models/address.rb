a27e81d44fb18666b93739796ba2843d43d3c360
Benji Thompson <thompson.benji@gmail.com>
2023-05-21 03:02:22 -0400

    refactor[models]: satisfy rubocop inverse_of rule

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
+# models/address.rb
 class Address < ApplicationRecord
-  has_many :sent_transactions, class_name: 'Transaction', foreign_key: 'from_address', primary_key: 'address'
-  has_many :received_transactions, class_name: 'Transaction', foreign_key: 'to_address', primary_key: 'address'
+  has_many :sent_transactions, class_name: 'Transaction', foreign_key: 'from_address', primary_key: 'address', inverse_of: :from_address
+  has_many :received_transactions, class_name: 'Transaction', foreign_key: 'to_address', primary_key: 'address', inverse_of: :to_address
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
+# models/transaction.rb
 class Transaction < ApplicationRecord
-  belongs_to :from_address, class_name: 'Address', foreign_key: 'from_address', primary_key: 'address'
-  belongs_to :to_address, class_name: 'Address', foreign_key: 'to_address', primary_key: 'address'
+  belongs_to :from_address, class_name: 'Address', foreign_key: 'from_address', primary_key: 'address', inverse_of: :sent_transactions
+  belongs_to :to_address, class_name: 'Address', foreign_key: 'to_address', primary_key: 'address', inverse_of: :received_transactions
 
   validates :cornlet_amount, numericality: { less_than_or_equal_to: :sender_balance }
 
