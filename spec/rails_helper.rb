c1d6cbb4e6bd9aaebc0934ee9efcf4bdd875588c
Benji Thompson <thompson.benji@gmail.com>
2023-05-21 02:35:21 -0400

    refactor[rails_helper.rb]: use .join for file/path to satisfy rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+# spec/rails_helper.rb
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -32,7 +33,7 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = Rails.root.join('spec/fixtures')
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
