b7af946d091d8b8323daaf88515b6b0ada036ca3
Benji Thompson <thompson.benji@gmail.com>
2023-05-21 00:57:56 -0400

    refactor[daily_stake_reward_job.rb]: reduce abc complexity for rubocop

--- app/jobs/daily_stake_reward_job.rb
+++ app/jobs/daily_stake_reward_job.rb
@@ -10,23 +10,36 @@ class DailyStakeRewardJob < ApplicationJob
 
     # For each address with cornlet_balance >= 10_000_000 except Satoshi, generate a stake reward transaction.
     Address.where('cornlet_balance >= ? AND address <> ?', 10_000_000, 'satoshi kozuka').find_each do |address|
-      ActiveRecord::Base.transaction do
-        transaction = Transaction.new(
-          from_address: satoshi,
-          to_address: address,
-          cornlet_amount: 25_000_000
-        )
-
-        transaction.save!
-
-        address.cornlet_balance += 25_000_000
-        address.save!
-
-        satoshi.cornlet_balance -= 25_000_000
-        satoshi.save!
-      rescue ActiveRecord::RecordInvalid => e
-        Rails.logger.info "Failed to save transaction for #{address.address}. Errors: #{e}"
-      end
+      process_address(address, satoshi)
     end
+  end
+
+  private
+
+  def process_address(address, satoshi)
+    ActiveRecord::Base.transaction do
+      create_transaction(address, satoshi)
+      update_balances(address, satoshi)
+    rescue ActiveRecord::RecordInvalid => e
+      Rails.logger.info "Failed to save transaction for #{address.address}. Errors: #{e}"
+    end
+  end
+
+  def create_transaction(address, satoshi)
+    transaction = Transaction.new(
+      from_address: satoshi,
+      to_address: address,
+      cornlet_amount: 25_000_000
+    )
+
+    transaction.save!
+  end
+
+  def update_balances(address, satoshi)
+    address.cornlet_balance += 25_000_000
+    address.save!
+
+    satoshi.cornlet_balance -= 25_000_000
+    satoshi.save!
   end
 end
