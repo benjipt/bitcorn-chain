e3d9b884961043d75a2e43c8310b8b81e679aec8
Benji Thompson <thompson.benji@gmail.com>
2023-05-23 22:35:11 -0400

    refactor[daily_stake_reward_job.rb]: rename variable satoshi -> seed_address

--- app/jobs/daily_stake_reward_job.rb
+++ app/jobs/daily_stake_reward_job.rb
@@ -2,32 +2,32 @@ class DailyStakeRewardJob < ApplicationJob
   queue_as :default
 
   def perform
-    satoshi = Address.find_by(address: 'satoshi kozuka')
-    unless satoshi && satoshi.cornlet_balance >= 25_000_000
+    seed_address = Address.find_by(address: 'satoshi kozuka')
+    unless seed_address && seed_address.cornlet_balance >= 25_000_000
       Rails.logger.info "Satoshi Kozuka's address does not exist or doesn't have enough balance."
       return
     end
 
     # For each address with cornlet_balance >= 10_000_000 except Satoshi, generate a stake reward transaction.
     Address.where('cornlet_balance >= ? AND address <> ?', 10_000_000, 'satoshi kozuka').find_each do |address|
-      process_address(address, satoshi)
+      process_address(address, seed_address)
     end
   end
 
   private
 
-  def process_address(address, satoshi)
+  def process_address(address, seed_address)
     ActiveRecord::Base.transaction do
-      create_transaction(address, satoshi)
-      update_balances(address, satoshi)
+      create_transaction(address, seed_address)
+      update_balances(address, seed_address)
     rescue ActiveRecord::RecordInvalid => e
       Rails.logger.info "Failed to save transaction for #{address.address}. Errors: #{e}"
     end
   end
 
-  def create_transaction(address, satoshi)
+  def create_transaction(address, seed_address)
     transaction = Transaction.new(
-      from_address: satoshi,
+      from_address: seed_address,
       to_address: address,
       cornlet_amount: 25_000_000
     )
@@ -35,11 +35,11 @@ class DailyStakeRewardJob < ApplicationJob
     transaction.save!
   end
 
-  def update_balances(address, satoshi)
+  def update_balances(address, seed_address)
     address.cornlet_balance += 25_000_000
     address.save!
 
-    satoshi.cornlet_balance -= 25_000_000
-    satoshi.save!
+    seed_address.cornlet_balance -= 25_000_000
+    seed_address.save!
   end
 end
