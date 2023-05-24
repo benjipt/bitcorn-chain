fa19ad136ac605c166c43e6457e4c69380ea232b
Benji Thompson <thompson.benji@gmail.com>
2023-05-23 22:54:58 -0400

    chore[application_record.rb]: add documentation comments

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
+# ActiveRecord::Base is the super class of all models in Rails.
+# We can define a primary abstract class for all models to inherit from.
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
